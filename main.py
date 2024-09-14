48138a76c57a521d15ff27ae7e8672ded0fd5584
Nobel Nizam F <163531708+NobelNizam@users.noreply.github.com>
2024-09-14 22:36:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Proyek Belajar
+Proyek Number   : 02
+Proyek Desc     : Simple RPG Game on Terminal
+
+Consepth :
+1. Ref: Undertale
+2. Berbasis pada opsi player
+3. Teks Only
+4. Menggunakan OOP
+
+'''
+
+
+
+
